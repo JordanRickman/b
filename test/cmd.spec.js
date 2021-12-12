ab9dc40a5463d20bbf64ee22b3f2abc93210a538
Jordan Rickman <jordan.rickman.42@gmail.com>
2021-12-12 03:51:39 +0000

    WIP: Really implement q.

--- test/cmd.spec.js
+++ test/cmd.spec.js
@@ -1,7 +1,7 @@
-const { startCmd } = require('../lib/cmd')
+const { runCommand } = require('../lib/cmd')
 
 
-describe('startCmd', () => {
+describe('runCommand', () => {
   let cmd
   afterEach(() => {
     // Try to prevent "a worker thread has failed to exit gracefully"
@@ -10,31 +10,31 @@ describe('startCmd', () => {
     }
   })
   it('returns a Promise', () => {
-    cmd = startCmd('echo > /dev/null')
+    cmd = runCommand('echo > /dev/null')
     expect(cmd).toBeInstanceOf(Promise)
   })
   it('captures the status code in the "status" attribute', async () => {
-    cmd = startCmd('exit 123')
+    cmd = runCommand('exit 123')
     const { status } = await cmd
     expect(status).toEqual(123)
   })
   it('captures the process id in the "pid" attribute', async () => {
-    cmd = startCmd('echo > /dev/null')
+    cmd = runCommand('echo > /dev/null')
     const { pid } = await cmd
     expect(typeof pid).toBe('number')
   })
   it('captures the stdout as a string', async () => {
-    cmd = startCmd('echo "Hello, world!"')
+    cmd = runCommand('echo "Hello, world!"')
     const { stdout } = await cmd
     expect(stdout).toBe('Hello, world!\n')
   })
   it('captures the stderr as a string', async () => {
-    cmd = startCmd('echo "Hello, world!" >&2')
+    cmd = runCommand('echo "Hello, world!" >&2')
     const { stderr } = await cmd
     expect(stderr).toBe('Hello, world!\n')
   })
   it('captures the termination signal if terminated', async () => {
-    cmd = startCmd('sleep 1')
+    cmd = runCommand('sleep 1')
     setTimeout(() => { 
       process.kill(cmd.childProcess.pid, 'SIGTERM')
     }, 50)
@@ -43,7 +43,7 @@ describe('startCmd', () => {
   })
   it('captures the error object if the spawn() failed', async () => {
     // extra options are sent to spawn()
-    cmd = startCmd('./nosuchfile', { shell: false })
+    cmd = runCommand('./nosuchfile', { shell: false })
     const { error } = await cmd
     // child_process errors are not always the same Error class, idk why
     // expect(error).toBeInstanceOf(Error)
@@ -52,12 +52,12 @@ describe('startCmd', () => {
   })
   it('resolves immediately if the process is spawned detached', async () => {
     let finished = false
-    cmd = startCmd('sleep 1', { detached: true }).then(() => { finished = true })
+    cmd = runCommand('sleep 1', { detached: true }).then(() => { finished = true })
     setTimeout(() => expect(finished).toBe(true))
   })
   // Skip test: works interactively but I can't do it via process.stdin
   it.skip('pipes stdin to the child process', async () => {
-    cmd = startCmd('read x ; echo $x')
+    cmd = runCommand('read x ; echo $x')
     setTimeout(() => process.stdin.write('yes'), 100)
     const { stdout } = await cmd
     expect(stdout).toBe('yes')
