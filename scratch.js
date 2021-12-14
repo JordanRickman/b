b0369860ea176a007d3b622ce511aa4854ff64c2
Jordan Rickman <jordan.rickman.42@gmail.com>
2021-12-14 05:32:52 +0000

    Bugfix: Stdin was keeping the parent process open.

--- scratch.js
+++ scratch.js
@@ -1,37 +1,5 @@
-const { Queue } = require('./lib/queue')
+const b = require('./index')
 
-const makeTask = (label, ms = 0, rejects = false) => {
-  return () => new Promise((resolve, reject) => {
-    if (ms > 0) {
-      setTimeout(() => {
-        console.log(label)
-        if (rejects) {
-          reject(label)
-        } else {
-          resolve(label)
-        }
-      }, ms)
-    } else {
-      setImmediate(() => {
-        console.log(label)
-        if (rejects) {
-          reject(label)
-        } else {
-          resolve(label)
-        }
-      })
-    }
-  })
-}
-
-t1 = makeTask('slow 1', 1000)
-t2 = makeTask('slow 2', 1000)
-t3 = makeTask('fast')
-console.log('made.')
-
-const q1 = new Queue()
-const q2 = new Queue()
-q1.pushTask(t1)
-q1.pushTask(t2)
-q2.pushTask(t3)
-console.log('started.')
+b`echo Hello,`
+b`read x; export x`
+b`echo $x`
