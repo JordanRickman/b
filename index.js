bbeed19c81e25df9acd673eef654f5b184294b15
Jordan Rickman <jordan.rickman.42@gmail.com>
2021-12-14 02:14:34 +0000

    Make index export b.

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const b = require('./lib/b');
+const b = require('./lib/b')
 
-(async () => {
-  b.with({ bg: true })`sleep 1; echo 'Hello, world' > FILE1`
-  b`cat FILE1 > FILE2`
-  console.log(`File contents: ${(await b`cat FILE2`).stdout}`)
-  b`rm FILE1`
-  b`rm FILE2`
-})()
+module.exports = b
