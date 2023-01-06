37a77346fc73cf53a6bd2eeb068fd35c42d8f067
Jordan Rickman <jordan.rickman.42@gmail.com>
2023-01-06 17:35:18 -0500

    Bugfix: Type declarations.

--- index.d.ts
+++ index.d.ts
@@ -24,25 +24,25 @@ interface BResult {
 }
 
 
-type BInstance = (strings: string[], ...interpolations: any[]) => BResult &
+type BInstance = ((strings: TemplateStringsArray, ...interpolations: any[]) => BResult) &
 {
   with(opts: BOpts, callback?: (b: BInstance) => void): BInstance;
-  fork(strings: string[], ...interpolations: any[]): BResult;
+  fork(strings: TemplateStringsArray, ...interpolations: any[]): BResult;
   fork(callback: (forkedInstance: BInstance) => void): BInstance;
   fork(): BInstance;
   cd(path: string): BInstance;
   env(environment: { [variableName: string]: string }): BInstance;
-  mayfail(strings: string[], ...interpolations: any[]): BResult;
-  bg(strings: string[], ...interpolations: any[]): BResult;
+  mayfail(strings: TemplateStringsArray, ...interpolations: any[]): BResult;
+  bg(strings: TemplateStringsArray, ...interpolations: any[]): BResult;
   user(username: string): BInstance;
   group(groupname: string): BInstance;
   uid(userid: number): BInstance;
   gid(groupid: number): BInstance;
   timeout(ms: number): BInstance;
-  quiet(strings: string[], ...interpolations: any[]): BResult;
-  silent(strings: string[], ...interpolations: any[]): BResult;
-  echo(strings: string[], ...interpolations: any[]): BResult;
-  stdoutof(strings: string[], ...interpolations: any[]): string;
+  quiet(strings: TemplateStringsArray, ...interpolations: any[]): BResult;
+  silent(strings: TemplateStringsArray, ...interpolations: any[]): BResult;
+  echo(strings: TemplateStringsArray, ...interpolations: any[]): BResult;
+  stdoutof(strings: TemplateStringsArray, ...interpolations: any[]): string;
   waitAll(): Promise<void>
 }
 
@@ -52,6 +52,7 @@ declare const b: BInstance & {
   raw(s: string): _SpecialInterpolate;
   squote(s: string): _SpecialInterpolate;
 }
+export = b
 
 declare class BShellError extends Error {
   name: 'BShellError';
