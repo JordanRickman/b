b57e262281c9c7a5ef8d415a619ffb1d3025a62d
Jordan Rickman <jordan.rickman.42@gmail.com>
2023-01-11 22:16:02 -0500

    Fix a bug in the Queue.

--- test/queue.spec.js
+++ test/queue.spec.js
@@ -1,4 +1,4 @@
-const { chainTask } = require('../lib/queue');
+const { Queue } = require('../lib/queue');
 
 
 const makeTasks = n => {
@@ -13,56 +13,72 @@ const makeTasks = n => {
   return [taskArray, taskHappenedArray]
 }
 
-const queueTasksInSequence = taskArray => {
-  const taskPromiseArray = new Array(taskArray.length).fill(null);
+const queueTasksInSequence = (queue, taskArray) => {
+  const taskPromiseArray = new Array(taskArray.length).fill(null)
   taskArray.forEach((task, i) => {
-    taskPromiseArray[i] = chainTask(taskPromiseArray[i-1], task)
+    taskPromiseArray[i] = queue.pushTask(task)
   })
-  return taskPromiseArray;
+  return taskPromiseArray
 }
 
-describe('chainTask', () => {
+describe('Queue', () => {
+  let queue
+  beforeEach(() => {
+    queue = new Queue()
+  })
   it('run tasks asynchronously in sequence', async () => {
     const [taskArray, taskHappenedArray] = makeTasks(3)
-    const taskPromiseArray = queueTasksInSequence(taskArray)
-    taskPromiseArray.map(async (taskPromise, i) => {
+    const taskPromiseArray = queueTasksInSequence(queue, taskArray)
+    return Promise.all(taskPromiseArray.map(async (taskPromise, i) => {
       expect(taskHappenedArray[i]).toBe(null);
       await taskPromise;
       expect(taskHappenedArray[i]).toBe(`Ran ${i}`);
-    })
+    }))
   })
   it('lets you attach other tasks in between', async () => {
     const [taskArray, taskHappenedArray] = makeTasks(3)
-    const taskPromiseArray = queueTasksInSequence(taskArray)
-    taskPromiseArray[1].then(() => {
+    const taskPromiseArray = queueTasksInSequence(queue, taskArray)
+    return taskPromiseArray[1].then(() => {
       expect(taskHappenedArray[1]).toBe(`Ran ${1}`) // task has happened
       expect(taskHappenedArray[2]).toBe(null) // next task hasn't happened yet
     })
   })
   it('resolves each promise with the task\'s return value', async () => {
     const [taskArray, taskHappenedArray] = makeTasks(3)
-    const taskPromiseArray = queueTasksInSequence(taskArray)
-    taskPromiseArray.map(async (taskPromise, i) => {
+    const taskPromiseArray = queueTasksInSequence(queue, taskArray)
+    return Promise.all(taskPromiseArray.map(async (taskPromise, i) => {
       expect(await taskPromise).toBe(`Result ${i}`);
-    })
+    }))
   })
   it('rejects all subsequent promises if one task fails', async () => {
     const [taskArray, taskHappenedArray] = makeTasks(3)
     taskArray.unshift(() => { throw new Error('Error 0') })
-    const taskPromiseArray = queueTasksInSequence(taskArray)
-    taskPromiseArray.forEach(async p => {
+    const taskPromiseArray = queueTasksInSequence(queue, taskArray)
+    return Promise.all(taskPromiseArray.map(async p => {
       await expect(p).rejects.toThrow('Error 0')
-    })
+    }))
   })
   it('does not run later tasks if one task fails', async () => {
     const [taskArray, taskHappenedArray] = makeTasks(3)
     taskArray.unshift(() => { throw new Error('Error 0') })
-    const taskPromiseArray = queueTasksInSequence(taskArray)
+    const taskPromiseArray = queueTasksInSequence(queue, taskArray)
     try {
       await taskPromiseArray[taskPromiseArray.length - 1]
     } catch (error) {
       // We expect an error.
     }
     expect(taskHappenedArray).toEqual([null, null, null])
+  })
+  it('allows recovery from a failed task', async () => {
+    const [taskArray, taskHappenedArray] = makeTasks(3)
+    try {
+      await queue.pushTask(() => { throw new Error('Error') })
+      throw new Error('queue.pushTask() on a failing function should have rejected.')
+    } catch (e) {
+      const taskPromiseArray = queueTasksInSequence(queue, taskArray)
+      return Promise.all(taskPromiseArray.map(async (taskPromise, i) => {
+        expect(await taskPromise).toBe(`Result ${i}`);
+      }))
+    }
   })
 })
